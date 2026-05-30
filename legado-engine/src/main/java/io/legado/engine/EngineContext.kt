eb9f921b0a1e8fd32e2e88423e03c82c09837200
240xu <2401826246@qq.com>
2026-05-30 13:30:59 +0800

    add legado-engine/src/main/java/io/legado/engine/EngineContext.kt

---
+++ legado-engine/src/main/java/io/legado/engine/EngineContext.kt
@@ -0,0 +1,26 @@
+﻿package io.legado.engine
+
+import io.legado.engine.provider.CacheProvider
+import io.legado.engine.provider.ConfigProvider
+import io.legado.engine.provider.LoginProvider
+
+/**
+ * 引擎上下文 - 持有外部注入的 Provider 引用
+ * 由 LNR 插件在初始化时设置
+ */
+object EngineContext {
+    var configProvider: ConfigProvider? = null
+    var loginProvider: LoginProvider? = null
+    var cacheProvider: CacheProvider? = null
+
+    fun init(config: ConfigProvider?, login: LoginProvider?, cache: CacheProvider?) {
+        configProvider = config
+        loginProvider = login
+        cacheProvider = cache
+    }
+
+    /** 检查功能开关 */
+    fun isFeatureEnabled(key: String, default: Boolean = false): Boolean {
+        return configProvider?.getBoolean(key, default) ?: default
+    }
+}
