89ca418256db55cbc6b875f0dd27c9ddb2ee6fe5
240xu <2401826246@qq.com>
2026-05-30 13:31:24 +0800

    add legado-engine/src/main/java/io/legado/engine/entity/rule/ContentRule.kt

---
+++ legado-engine/src/main/java/io/legado/engine/entity/rule/ContentRule.kt
@@ -0,0 +1,2 @@
+package io.legado.engine.entity.rule
+data class ContentRule(var content:String?=null,var title:String?=null,var src:String?=null,var imageDecode:String?=null,var replaceRegex:String?=null,var webJs:String?=null,var payAction:String?=null,var sourceRegex:String?=null)
